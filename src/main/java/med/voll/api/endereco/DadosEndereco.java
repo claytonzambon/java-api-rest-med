1c83c10a6a80ff12fe9395dc97f8002ac587651f
Clayton Zambon <zambon@outlook.com.br>
2024-01-28 16:06:20 -0300

    FEAT: Validação com Bean Validation

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,9 +1,20 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEndereco(
+        @NotBlank
         String logradouro,
-        String bairro, String cep,
-        String cidade, String uf,
+        @NotBlank
+        String bairro,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        @NotBlank
+        String cidade,
+        @NotBlank
+        String uf,
         String complemento,
         String numero) {
 
