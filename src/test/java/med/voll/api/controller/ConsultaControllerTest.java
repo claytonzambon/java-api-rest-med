22b72628a3b121f7aa2db9187700f02011e076b0
Clayton Zambon <zambon@outlook.com.br>
2024-03-01 22:40:25 -0300

    TEST: Testando erro 400 - Parte 03

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -1,5 +1,7 @@
 package med.voll.api.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,4 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ConsultaControllerTest {
 
+    @Test
+    @DisplayName("")
+    void agendar_cenario1(){
+
+    }
 }
