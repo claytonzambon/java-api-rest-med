62cd5933cb32d9445bf1b970d2112c567231fd25
Clayton Zambon <zambon@outlook.com.br>
2024-03-03 22:43:54 -0300

    TEST: Testando erro 400 - Parte 05

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -2,12 +2,19 @@ package med.voll.api.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ConsultaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     @DisplayName("Deveria devolver codigo 400 quando informacoes estao invalidas")
